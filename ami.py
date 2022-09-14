55ae22ad6730ffb92e408a95bd0df5360256a089
godlovenda <113544300+godlovenda@users.noreply.github.com>
2022-09-14 14:21:58 -0400

    Create ami.py

---
+++ ami.py
@@ -0,0 +1,4 @@
+
+# amiFilter=[{"Name":"owner-id","Values":["762873164855"]},{"Name":"name","Values":["ops-base-master-*"]},{region: 'us-west-2'}]
+# currentImageName=unknown
+base_image_id = "ami-04a726a285fbf6c8d"
