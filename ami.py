3220f614e83747ade5c659612b8687b6d67bdcf4
godlovenda <113544300+godlovenda@users.noreply.github.com>
2022-09-14 14:27:33 -0400

    Update ami.py

--- ami.py
+++ ami.py
@@ -1,4 +1,9 @@
 
-# amiFilter=[{"Name":"owner-id","Values":["762873164855"]},{"Name":"name","Values":["ops-base-master-*"]},{region: 'us-west-2'}]
+# With AMI name mentioned in the comments
+# amiFilter=[{"Name":"owner-id","Values":["602401143452"]},{"Name":"name","Values":["amazon-eks-node-1.21-*"]}]
 # currentImageName=unknown
-base_image_id = "ami-04a726a285fbf6c8d"
+my_ami1: ami-02ce3d9008cab69cb
+# Only AMI, no name mentioned
+# amiFilter=[{"Name":"owner-id","Values":["602401143452"]},{"Name":"name","Values":["amazon-eks-node-1.20-*"]}]
+# currentImageName=unknown
+my_ami2: ami-0083e9407e275acf2
